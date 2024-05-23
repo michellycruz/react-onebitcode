01bbed0c36fbfff7cbc5f033df406ce2782d08c4
Mich <125368412+michellycruz@users.noreply.github.com>
2024-05-22 23:55:15 -0300

    exercicio

--- exercicio1-04/exercicio01/src/App.jsx
+++ exercicio1-04/exercicio01/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App(){
+  return(
+    <div>
+        <img src="/react.svg" alt="" />
+        <h1>React</h1>
+        <p>A biblioteca para interfaces de usuário web e nativas</p>
+        <button>Aprenda React</button>
+        <button>Referência da API</button>
+        <hr />
+        <h2>Crie interfaces de usuário de componentes</h2>
+        <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes.</p>
+        <hr />
+        <h2>Escreva componentes com código e marcação</h2>
+        <p>Componentes React são funções Javascript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe Javascript popularizada pelo React.</p>
+        <hr />
+        <h2>Próximos passos</h2>
+        <ul>
+          <li>Uso de dados dinâmicos no JSX</li>
+          <li>Criação de novos componentes</li>
+          <li>Estilização de componentes</li>
+          <li>Reutilização de componentes</li>
+          <li>Uso de props e children</li>
+          <li>Uso de eventos do javascript</li>
+        </ul>
+    </div>
   )
 }
-
-export default App
