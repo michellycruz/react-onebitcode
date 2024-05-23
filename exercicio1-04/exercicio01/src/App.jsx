689f5e798c21dee4d004b068acab7149b2952e84
Mich <125368412+michellycruz@users.noreply.github.com>
2024-05-23 00:05:33 -0300

    Update App.jsx

--- exercicio1-04/exercicio01/src/App.jsx
+++ exercicio1-04/exercicio01/src/App.jsx
@@ -1,11 +1,13 @@
 export default function App(){
   return(
-    <div>
-        <img src="/react.png" alt="" />
+    <div className="app">
+        <img src="/react.png" alt="logo do react" />
         <h1>React</h1>
         <p>A biblioteca para interfaces de usuário web e nativas</p>
-        <button>Aprenda React</button>
-        <button>Referência da API</button>
+        <div>
+          <button>Aprenda React</button>
+          <button>Referência da API</button>
+        </div>
         <hr />
         <h2>Crie interfaces de usuário de componentes</h2>
         <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes.</p>
