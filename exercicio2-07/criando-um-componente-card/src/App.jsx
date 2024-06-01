45a284ab814bd12a1b800caed6ed2f5cd3e8cc29
Mich <125368412+michellycruz@users.noreply.github.com>
2024-05-31 21:33:52 -0300

    como utilizar props

--- exercicio2-07/criando-um-componente-card/src/App.jsx
+++ exercicio2-07/criando-um-componente-card/src/App.jsx
@@ -1,17 +1,16 @@
 import Card from "./components/Card"
+import swPosterImg from "./assets/starwars.jpg"
+import emPosterImg from "./assets/empire.jpg"
+import retPosterImg from "./assets/return.jpg"
 
 function App() {
   return (
       <>
-         <Card />
-         <Card />
-         <Card />
-         <Card />
-         <Card />
-         <Card />
-         <Card />
+         <Card title="Pôster: Star Wars (1977)" posterImg={swPosterImg} /> 
+         <Card title="Pôster: Empire Strikes Back (1980)" posterImg={emPosterImg} />
+         <Card title="Pôster: Return of Jedi (1983)" posterImg={retPosterImg} />
       </> // <-- abreviação da tag <fragment></fragment>
-  )
+  )// "title= '' " utilização da props criada no arquivo index da pasta card
 }
 
 export default App
--- exercicio2-07/criando-um-componente-card/src/components/Card/index.jsx
+++ exercicio2-07/criando-um-componente-card/src/components/Card/index.jsx
@@ -1,16 +1,16 @@
-import posterImg from "../../assets/starwars.jpg"
+/* eslint-disable react/prop-types */
 import Button from "../Button"
 import styles from "./styles.module.css"
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className={styles.container}>
-            <img className={styles.poster} src={posterImg} alt="Starwars poster" />
+            <img className={styles.poster} src={props.posterImg} alt={props.title} />
             <div>
-                <h2 className={styles.titulo}>Pôster: Star Wars (1977)</h2>
+                <h2 className={styles.titulo}>{props.title}</h2>
                 <p className={styles.paragrafo}>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
                 <Button />
             </div>
         </div>
     )
-}
+} // aula 18 - minuto 8:12 - o que são e como uti
