564108f69da46cca76d1db9b5acc27f04a5c8468
Mich <125368412+michellycruz@users.noreply.github.com>
2024-10-09 19:44:41 -0300

    rendenização condicional

--- exercicio4-10/react-exercicio-4/src/App.jsx
+++ exercicio4-10/react-exercicio-4/src/App.jsx
@@ -4,7 +4,10 @@ import Input from "./components/input"
 function App() {
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
-  const [passwordSize, setPasswordSize] = useState(12)
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
+
+  const passwordSize = showInput ? customSize : 8
 
   function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
@@ -27,9 +30,20 @@ function App() {
       <div className="app">
         <h1>Gerador de Senhas</h1>
         <div>
-          <label htmlFor="passwordSize">Tamanho:</label>
-          <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+          <label htmlFor="showInput">Customizar tamanho</label>
+          <input 
+            type="checkbox"
+            id="showInput"
+            value={showInput}
+            onChange={() => setShowInput(currentState => !currentState)}
+          />
         </div>
+        {showInput ? (
+          <div>
+            <label htmlFor="passwordSize">Tamanho:</label>
+            <Input passwordSize={customSize} setPasswordSize={setCustomSize} />
+          </div>
+        ) : null}
         <button onClick={generate}>Gerar senha de {passwordSize} caracteres </button>
         <button onClick={copyToClipboard}>{copyText}</button>
         <div>{password}</div>
