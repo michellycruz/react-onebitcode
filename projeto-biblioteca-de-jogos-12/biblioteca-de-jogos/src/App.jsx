e3fc91689688ba402fd5aece0eae20cf06b17523
Mich <125368412+michellycruz@users.noreply.github.com>
2024-10-10 16:17:22 -0300

    biblioteca de jogos, separação de componentes e organização do código

--- projeto-biblioteca-de-jogos-12/biblioteca-de-jogos/src/App.jsx
+++ projeto-biblioteca-de-jogos-12/biblioteca-de-jogos/src/App.jsx
@@ -1,68 +1,28 @@
-import { useState } from "react"
+import Game from "./components/Game"
+import NewGameForm from "./components/NewGameForm"
+import useGameCollection from "./hooks/useGameCollections"
 
-function App() {
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if (!storedGames) return []
-    return JSON.parse(storedGames)
-  })
-
-  const [title, setTitle] = useState("")
-  const [cover, setCover] = useState("")
-
-  const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 1000000)
-    const game = { id, title, cover }
-    setGames(state => {
-      const newState = [...state, game]
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-  const removeGame = (id) => {
-    setGames(state => {
-      const newState = state.filter(game => game.id !== id)
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-  const handleSubmit = (ev) => {
-    ev.preventDefault()
-    addGame({ title, cover })
-    setTitle("")
-    setCover("")
-  }
+export default function App() {
+  const { games, addGame, removeGame } = useGameCollection()
 
   return (
-    <div className="app">
+    <div id="app">
       <h1>Biblioteca de Jogos</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Título:</label>
-          <input type="text" id="title" value={title} onChange={(ev) => setTitle(ev.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="cover">Capa:</label>
-          <input type="text" id="cover" value={cover} onChange={(ev) => setCover(ev.target.value)} />
-        </div>
-        <button>Adicionar</button>
-      </form>
+      <NewGameForm addGame={addGame}/>
       <div className="games">
-        {games.map((game) => (
-          <div key={game.id}>
-            <img src={game.cover} alt="Capa do jogo" />
-            <div>
-              <h2>{game.title}</h2>
-              <button onClick={() => removeGame(game.id)}>
-                Remover
-              </button>
-            </div>
-          </div>))}
+        {games.length > 0 ? 
+          games.map((game) => (
+            <Game 
+              key = {game.id} 
+              title = {game.title} 
+              cover = {game.cover} 
+              onRemove = {() => removeGame(game.id)}
+            / >)) 
+            
+          : (
+            <h2 style={{margin: "4rem auto"}}>Parece que ainda não tem nada aqui, tente adicionar alguns jogos.</h2>
+          )}
       </div>
     </div>
   )
 }
-
-export default App
