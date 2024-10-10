3ebab69db2315308ff55f4403a7ba2126969f863
Mich <125368412+michellycruz@users.noreply.github.com>
2024-10-10 18:06:28 -0300

    persistindo valores com o useref

---
+++ conhecendo-useRef-14/conhecendo-o-useref/src/App.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react"
+import { useState } from "react"
+
+export default function App() {
+  let variable = 0
+  const [state, setState] = useState(0)
+  const ref = useRef(0)
+
+  const showValues = () => {
+    alert(`
+        Variável: ${variable}
+        Ref: ${ref.current}
+        State: ${state}
+      `)
+  }
+
+  return (
+    <div>
+      <h2>Conhecendo o useRef</h2>
+      <hr />
+      <p>Variável: {variable}</p>
+      <p>Ref: ${ref.current}</p>
+      <p>State: {state}</p>
+      <button onClick={() => variable++}>Aumentar a variável</button>
+      <button onClick={() => ref.current++}>Aumentar ref</button>
+      <button onClick={() => setState(state => state + 1)}>Aumentar o state</button>
+      <hr />
+      <button onClick={showValues}>Exibir valores</button>
+    </div>
+  )
+}
